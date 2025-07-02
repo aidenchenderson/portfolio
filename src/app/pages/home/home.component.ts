9c3d5284832dd6a6436c34fe0d1cd354762b6f45
Aiden Henderson <88954466+aidenchenderson@users.noreply.github.com>
2025-07-01 21:10:45 -0400

    Restructured my vision for the portfolio. Created: logo, project skeletion.

---
+++ src/app/pages/home/home.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-home',
+    imports: [],
+    templateUrl: './home.component.html',
+    styleUrl: './home.component.css'
+})
+export class HomeComponent implements OnInit{
+    constructor(
+
+    ) {
+
+    }
+
+    ngOnInit() {
+        
+    }
+}
