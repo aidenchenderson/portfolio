e1e2b281287d219e90710d51f4a0c9a1d5b685ea
Aiden Henderson <88954466+aidenchenderson@users.noreply.github.com>
2025-07-05 17:00:11 -0400

    Changing framework from Angular to NextJS

---
+++ src/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default async function Home() {
+  await new Promise((resolve) => setTimeout(resolve, 5000));
+  return (
+    <div>
+      Home page
+    </div>
+  );
+}
