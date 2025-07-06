76be71ddfae1f54122b1381bc6288025b5acd6e6
Aiden Henderson <88954466+aidenchenderson@users.noreply.github.com>
2025-07-05 21:43:24 -0400

    Completed initial setup for home page loading animation. I'm becoming a big NextJS fan.

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,3 +1,13 @@
+import styles from './glitch.module.css';
+
 export default function Loading() {
-    return <h1>LOADING ... </h1>;
+    return (
+        <div className="flex justify-center items-center h-screen">
+            <h1 aria-hidden="true" className={styles.glitch}>
+                <span>ahenderson.dev</span>
+                ahenderson.dev
+                <span>ahenderson.dev</span>
+            </h1>
+        </div>
+    );
 }
