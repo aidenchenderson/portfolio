97c05d6ecad39644ef4c341f79af447d14ecc308
Aiden Henderson <88954466+aidenchenderson@users.noreply.github.com>
2025-07-08 21:08:35 -0400

    added loading bar animation to hope page loading screen

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,13 +1,18 @@
 import styles from './glitch.module.css';
 
 export default function Loading() {
-    return (
-        <div className="flex justify-center items-center h-screen">
-            <h1 aria-hidden="true" className={styles.glitch}>
-                <span>ahenderson.dev</span>
-                ahenderson.dev
-                <span>ahenderson.dev</span>
-            </h1>
+  return (
+    <div className="flex justify-center items-center h-screen flex-col">
+      <div className="inline-block relative">
+        <h1 aria-hidden="true" className={`${styles.glitch}`}>
+          <span>ahenderson.dev</span>
+          ahenderson.dev
+          <span>ahenderson.dev</span>
+        </h1>
+        <div className={styles.bar}>
+          <div className={styles.load}></div>
         </div>
-    );
+      </div>
+    </div>
+  );
 }
