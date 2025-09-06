a1fe2d1477a882f41d8dc6e910a14877465ddc21
Aiden Henderson <88954466+aidenchenderson@users.noreply.github.com>
2025-09-06 13:35:54 -0400

    finished about page design

--- src/components/about/about.tsx
+++ src/components/about/about.tsx
@@ -1,6 +1,5 @@
-'use client';
-
 import styles from './about.module.css';
+import Links from '../links/links';
 
 export default function About() {
   return (
@@ -8,17 +7,20 @@ export default function About() {
     <div className='bg-[#151515] border-none rounded-tl-[3rem] rounded-tr-[3rem] mx-auto 
                     flex flex-col justify-center justify-items-center items-center h-screen w-screen'>
       {/* div for making a black circle */}
-      <div className='bg-[#0a0a0a] flex flex-col items-center'>
+      <div className={`${styles.circle} bg-[#0a0a0a] flex flex-col items-center justify-center content-center w-[500px] h-[500px] rounded-[50%]`}>
         <div className='flex flex-col items-center'>
           <p className={`${styles.connectTitle} mb-3 pt-3`}>Connect with Me</p>
-          <p className={`${styles.subText} w-4/10 text-center mb-5 pb-5`}>
+          <p className={`${styles.subText} w-1/2 text-center mb-2 pb-5`}>
             Whether you are a university student, industry professional, or you just want to say hi,
             feel free to email me!
           </p>
         </div>
-        <div className='pb-3'>
-          <button className={`${styles.emailButton} cursor-pointer`}>Email</button>
+        <div className='flex relative pb-3'>
+          <a href='mailto:aiden.henderson.c@gmail.com?subject=A.%20Henderson%20Website%20Visit'>
+            <button className={`${styles.emailButton} cursor-pointer`}>Email</button>
+          </a>
         </div>
+        <Links/>
       </div>
     </div>
   );
