f1b100ac89e9c06946528985888207ea7948dda7
Aiden Henderson <88954466+aidenchenderson@users.noreply.github.com>
2026-01-14 19:30:09 -0500

    add: header to mobile layout

--- src/pages/HomeMobileLayout.tsx
+++ src/pages/HomeMobileLayout.tsx
@@ -2,15 +2,16 @@ import '../styles/HomeMobileLayout.css';
 
 export default function HomeMobileLayout() {
   return (
-    <div className="relative min-h-screen w-full bg-[#050a10] overflow-hidden group">
+    <div className='relative min-h-screen w-full bg-[#050a10] overflow-hidden group'>
       {/* grid display */}
-      <div className="absolute inset-0 z-0 overflow-hidden grid-green background-layer"></div>
+      <div className='absolute inset-0 z-0 overflow-hidden grid-green background-layer'></div>
 
-      <div className='flex flex-col items-center py-11'>
+      <div className='flex flex-col items-center py-10'>
         {/* header */}
         <div className='header-wrapper cursor-default p-0.5 w-auto inline-block'>
-          <div className='header font-sans font-bold text-4xl'>
-            <p className="p-10">AHENDERSON.DEV</p>
+          <div className='header font-sans text-center'>
+            <p className='pt-5 font-bold text-3xl'>AHENDERSON.DEV</p>
+            <p className='pb-5 font-bold text-[1.05rem]'>CS Student & Software Developer</p>
           </div>
         </div>
       </div>
